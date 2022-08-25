20c612534313c2a3852bcaf7f84901d3ad0513ce
Encors <cheperish@mail.ru>
2022-08-25 19:34:23 +0300

    изменения в слайдере

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 $(window).on('load resize', function() {
     if ($(window).width() < 769) {
         $('.bikes__slider:not(.slick-initialized)').slick({
+          centerMode: true,
           dots: true,
           infinite: true,
           speed: 100,
-          arrows: false
+          arrows: false,
+          variableWidth: true
         });
       } else {
         $(".bikes__slider.slick-initialized").slick("unslick");
